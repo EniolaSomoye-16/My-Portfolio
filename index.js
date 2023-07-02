098e52f941a85ab444d977698464afbcc10bf549
Eniola Somoye <110903578+EniolaSomoye-16@users.noreply.github.com>
2023-07-02 01:10:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+//toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+}
+
+
+
+// function to check if an element is in the viewport
+  // Attach scroll event listener
+//   window.addEventListener('scroll', handleScroll);
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+            // active section for animations
+            sec.classList.add('show-animate');
+        }
+        // if you want to use animation that repeats on scroll
+        else{
+            sec.classList.remove('show-animate');
+        }
+    });
+    // sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle ison and navbar when clicking on navbar links(scroll)
+    menuIcon.classList.remove('bx-x');
+    navBar.classList.remove('active');
+
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.
+    scrollHeight);
+}
